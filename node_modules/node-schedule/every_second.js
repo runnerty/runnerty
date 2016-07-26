6e3c6012bb48de1984b577d3635ec765e0d08eb2
carlosca <carloscarmonaalcantara@gmail.com>
2016-07-26 13:28:38 +0200

    Remove node_modules

--- node_modules/node-schedule/every_second.js
+++
@@ -1,9 +0,0 @@
-const schedule = require('./');
-
-const job1 = schedule.scheduleJob({ second : null }, () => {
-   console.log(new Date(), 'job1');
-});
-
-const job2 = schedule.scheduleJob('* * * * * *', () => {
-   console.log(new Date(), 'job2');
-});
--- node_modules/node-schedule/test_cancel.js
+++
@@ -1,9 +0,0 @@
-var cron = require('./');
-
-var j = cron.scheduleJob('nasr', '* * * * * *', function() {
-    console.log('will get this console on every 5 sec.');
-});
-var k = cron.scheduleJob('30 * * * * *', function() {
-    cron.scheduledJobs['nasr'].cancel();
-    console.log('at first 30 sec of a minute the above job will get cancelled.');
-});
