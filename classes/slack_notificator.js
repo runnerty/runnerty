dc83bb283ee9f5d6affeef9d68253b2165c90de6
carlosca <carloscarmonaalcantara@gmail.com>
2016-10-06 18:09:43 +0200

    Slack messages in order

--- classes/slack_notificator.js
+++ classes/slack_notificator.js
@@ -1,16 +1,51 @@
 "use strict";
 
-var Slack           = require('slack-node');
-var logger          = require("../libs/utils.js").logger;
-var replaceWith     = require("../libs/utils.js").replaceWith;
-var Notification    = require("./notification.js");
+var logger           = require("../libs/utils.js").logger;
+var replaceWith      = require("../libs/utils.js").replaceWith;
+var Notification     = require("./notification.js");
+var IncomingWebhooks = require('@slack/client').IncomingWebhook;
 
+var pendings = [];
+
+function sendNextPending(){
+  return new Promise((resolve) => {
+    var actualNotification = pendings.shift();
+    var wh = new IncomingWebhooks(actualNotification.webhookurl);
+    wh.send({
+      text: actualNotification.msgToSend,
+      channel: actualNotification.channel,
+      iconEmoji: actualNotification.bot_emoji,
+      username: actualNotification.bot_name
+    }, function () {
+     resolve();
+    });
+  });
+};
+
+function slackSender(){
+  if(pendings.length){
+    sendNextPending()
+      .then(() => {
+        slackSender();
+      })
+      .catch(function(err){
+        logger.log('error',`Chain ${_this.id} loadEvents: `+err);
+        resolve();
+      });
+  }else{
+    setTimeout(function(){
+      slackSender();
+    },1000)
+  }
+};
+
+slackSender();
 
 class slackNotificator extends Notification{
-  constructor(type, id, token, bot_name, bot_emoji, message, channel, recipients){
+  constructor(type, id, webhookurl, bot_name, bot_emoji, message, channel, recipients){
     super('slack', id, null, message, recipients, null, null);
 
-    this.token = token;
+    this.webhookurl = webhookurl;
     this.bot_name = bot_name;
     this.bot_emoji = bot_emoji;
     this.channel = channel;
@@ -21,40 +56,26 @@ class slackNotificator extends Notification{
   }
 
   notificate(values){
+    var _this = this;
+
     return new Promise((resolve) => {
-
-      this.loadConfig()
+      _this.loadConfig()
       .then((configValues) => {
-      if (configValues){
-        if (!this.token && configValues.token)         this.token     = configValues.token;
-        if (!this.bot_name && configValues.bot_name)   this.bot_name  = configValues.bot_name;
-        if (!this.bot_emoji && configValues.bot_emoji) this.bot_emoji = configValues.bot_emoji;
-        if (!this.channel && configValues.channel)     this.channel   = configValues.channel;
-      }
-
-      var slack = new Slack(this.token);
-      var msg = replaceWith(this.message, values);
-
-      logger.log('debug','[SLACK NOTIFICATION] > '+msg);
-
-      slack.api('chat.postMessage', {
-        text: msg,
-        channel: this.channel,
-        username: this.bot_name,
-        icon_emoji: this.bot_emoji,
-      },function(err, response){
-        if(err){
-          logger.log('error','Slack notification: '+err);
-          logger.log('error','Slack notification: '+msg);
+        if (configValues){
+          if (!_this.webhookurl && configValues.webhookurl) _this.webhookurl = configValues.webhookurl;
+          if (!_this.bot_name && configValues.bot_name)   _this.bot_name  = configValues.bot_name;
+          if (!_this.bot_emoji && configValues.bot_emoji) _this.bot_emoji = configValues.bot_emoji;
+          if (!_this.channel && configValues.channel)     _this.channel   = configValues.channel;
         }
+        _this.msgToSend = replaceWith(_this.message, values);
+        pendings.push(_this);
+        resolve();
+      })
+      .catch(function(e){
+        logger.log('error','Slack notificate loadConfig '+e)
+        resolve();
       });
-      resolve();
-  })
-  .catch(function(e){
-      logger.log('error','Slack notificate loadConfig '+e)
-      resolve();
     });
-  });
   }
 }
 
