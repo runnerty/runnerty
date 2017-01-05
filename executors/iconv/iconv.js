6507f720d716fb3a3bc7196bf9ecb24adf90c720
carlos <carloscarmonaalcantara@gmail.com>
2017-01-06 00:49:15 +0100

    Executors Classes

--- executors/iconv/iconv.js
+++ executors/iconv/iconv.js
@@ -1,102 +1,111 @@
 "use strict";
 
-var logger = require("../../libs/utils.js").logger;
-var replaceWith = require("../../libs/utils.js").replaceWith;
 var iconv = require('iconv-lite');
 var fs = require('fs');
 var path = require('path');
 
-module.exports.exec = function (process) {
+var Execution = require("../../classes/execution.js");
 
-  return new Promise(function (resolve, reject) {
-    process.loadExecutorConfig()
-      .then((configValues) => {
+class iconvExecutor extends Execution {
+  constructor(process) {
+    super(process);
+  }
 
-        var _file_input = replaceWith(process.exec.file_input || configValues.file_input, process.values());
-        var _file_output = replaceWith(process.exec.file_output || configValues.file_output, process.values());
-        var _decode_encoding = replaceWith(process.exec.decode_encoding || configValues.decode_encoding, process.values());
-        var _encode_encoding = replaceWith(process.exec.encode_encoding || configValues.encode_encoding, process.values());
+  exec(process) {
+    var _this = this;
 
-        if (_decode_encoding && _encode_encoding) {
+    return new Promise(function (resolve, reject) {
+      process.loadExecutorConfig()
+        .then((configValues) => {
 
-          if (iconv.encodingExists(_decode_encoding) && iconv.encodingExists(_encode_encoding)) {
+          var _file_input = _this.replaceWith(process.exec.file_input || configValues.file_input, process.values());
+          var _file_output = _this.replaceWith(process.exec.file_output || configValues.file_output, process.values());
+          var _decode_encoding = _this.replaceWith(process.exec.decode_encoding || configValues.decode_encoding, process.values());
+          var _encode_encoding = _this.replaceWith(process.exec.encode_encoding || configValues.encode_encoding, process.values());
 
-            // CONVERTING FILE
-            if (_file_input && _file_output) {
-              var fileStream = fs.createReadStream(_file_input);
-              var fileStreamOutput = fs.createWriteStream(_file_output);
-              var decoderStream = iconv.decodeStream(_decode_encoding);
+          if (_decode_encoding && _encode_encoding) {
+
+            if (iconv.encodingExists(_decode_encoding) && iconv.encodingExists(_encode_encoding)) {
+
+              // CONVERTING FILE
+              if (_file_input && _file_output) {
+                var fileStream = fs.createReadStream(_file_input);
+                var fileStreamOutput = fs.createWriteStream(_file_output);
+                var decoderStream = iconv.decodeStream(_decode_encoding);
 
 
-              fileStream
-                .pipe(decoderStream)
-                .pipe(iconv.encodeStream(_encode_encoding))
-                .pipe(fileStreamOutput);
+                fileStream
+                  .pipe(decoderStream)
+                  .pipe(iconv.encodeStream(_encode_encoding))
+                  .pipe(fileStreamOutput);
 
 
-              fileStream.on('error', function (err) {
-                logger.log('error', 'Error Iconv reading file', _file_input, err);
-                process.execute_err_return = `Error Iconv reading file ${_file_input}: ${err}`;
+                fileStream.on('error', function (err) {
+                  _this.logger.log('error', 'Error Iconv reading file', _file_input, err);
+                  process.execute_err_return = `Error Iconv reading file ${_file_input}: ${err}`;
+                  process.execute_return = '';
+                  process.error();
+                  reject(process);
+                });
+
+                decoderStream.on('error', function (err, str) {
+                  _this.logger.log('error', `Error Iconv decoding (${_decode_encoding}) file ${_file_input}:`, err);
+                  process.execute_err_return = `Error Iconv decoding (${_decode_encoding}) file ${_file_input}: ${err}`;
+                  process.execute_return = '';
+                  process.error();
+                  reject(process);
+                });
+
+                decoderStream.on('error', function (err, str) {
+                  _this.logger.log('error', `Error Iconv encoding (${_encode_encoding}) file ${_file_input}:`, err);
+                  process.execute_err_return = `Error Iconv encoding (${_encode_encoding}) file ${_file_input}: ${err}`;
+                  process.execute_return = '';
+                  process.error();
+                  reject(process, err);
+                });
+
+                fileStreamOutput.on('error', function (err) {
+                  _this.logger.log('error', 'Error Iconv writing encoded file', _file_output, err);
+                  process.execute_err_return = `Error Iconv writing encoded file ${_file_output}: ${err}`;
+                  process.execute_return = '';
+                  process.error();
+                  reject(process);
+                });
+
+                fileStreamOutput.on('close', function () {
+                  process.execute_err_return = '';
+                  process.execute_return = '';
+                  process.end();
+                  resolve();
+                });
+              } else {
+                _this.logger.log('error', `Error Iconv files not setted: file_input: ${_file_input} / file_output: ${_file_output}`);
+                process.execute_err_return = `Error Iconv files not setted: file_input: ${_file_input} / file_output: ${_file_output}`;
                 process.execute_return = '';
                 process.error();
                 reject(process);
-              });
+              }
 
-              decoderStream.on('error', function (err, str) {
-                logger.log('error', `Error Iconv decoding (${_decode_encoding}) file ${_file_input}:`, err);
-                process.execute_err_return = `Error Iconv decoding (${_decode_encoding}) file ${_file_input}: ${err}`;
-                process.execute_return = '';
-                process.error();
-                reject(process);
-              });
-
-              decoderStream.on('error', function (err, str) {
-                logger.log('error', `Error Iconv encoding (${_encode_encoding}) file ${_file_input}:`, err);
-                process.execute_err_return = `Error Iconv encoding (${_encode_encoding}) file ${_file_input}: ${err}`;
-                process.execute_return = '';
-                process.error();
-                reject(process, err);
-              });
-
-              fileStreamOutput.on('error', function (err) {
-                logger.log('error', 'Error Iconv writing encoded file', _file_output, err);
-                process.execute_err_return = `Error Iconv writing encoded file ${_file_output}: ${err}`;
-                process.execute_return = '';
-                process.error();
-                reject(process);
-              });
-
-              fileStreamOutput.on('close', function () {
-                process.execute_err_return = '';
-                process.execute_return = '';
-                process.end();
-                resolve();
-              });
             } else {
-              logger.log('error', `Error Iconv files not setted: file_input: ${_file_input} / file_output: ${_file_output}`);
-              process.execute_err_return = `Error Iconv files not setted: file_input: ${_file_input} / file_output: ${_file_output}`;
+              _this.logger.log('error', `Error Iconv encodings not supported. decode_encoding: ${_decode_encoding} ${iconv.encodingExists(_decode_encoding) ? 'supported' : 'not supported'} / encode_encoding: ${_encode_encoding} ${iconv.encodingExists(_decode_encoding) ? 'supported' : 'not supported'}`);
+              process.execute_err_return = `Error Iconv encodings not supported. decode_encoding: ${_decode_encoding} ${iconv.encodingExists(_decode_encoding) ? 'supported' : 'not supported'} / encode_encoding: ${_encode_encoding} ${iconv.encodingExists(_decode_encoding) ? 'supported' : 'not supported'}`;
               process.execute_return = '';
               process.error();
               reject(process);
             }
 
           } else {
-            logger.log('error', `Error Iconv encodings not supported. decode_encoding: ${_decode_encoding} ${iconv.encodingExists(_decode_encoding) ? 'supported' : 'not supported'} / encode_encoding: ${_encode_encoding} ${iconv.encodingExists(_decode_encoding) ? 'supported' : 'not supported'}`);
-            process.execute_err_return = `Error Iconv encodings not supported. decode_encoding: ${_decode_encoding} ${iconv.encodingExists(_decode_encoding) ? 'supported' : 'not supported'} / encode_encoding: ${_encode_encoding} ${iconv.encodingExists(_decode_encoding) ? 'supported' : 'not supported'}`;
+            _this.logger.log('error', `Error Iconv encoders not setted. decode_encoding: ${_decode_encoding} / encode_encoding: ${_encode_encoding}`);
+            process.execute_err_return = `Error Iconv encoders not setted. decode_encoding: ${_decode_encoding} / encode_encoding: ${_encode_encoding}`;
             process.execute_return = '';
             process.error();
             reject(process);
           }
 
-        } else {
-          logger.log('error', `Error Iconv encoders not setted. decode_encoding: ${_decode_encoding} / encode_encoding: ${_encode_encoding}`);
-          process.execute_err_return = `Error Iconv encoders not setted. decode_encoding: ${_decode_encoding} / encode_encoding: ${_encode_encoding}`;
-          process.execute_return = '';
-          process.error();
-          reject(process);
-        }
+        });
+    });
 
-      });
-  });
+  }
+}
 
-};
+module.exports = iconvExecutor;
