af9527406613c3e69f6a07bd9c476cd432b2024f
carlos <carloscarmonaalcantara@gmail.com>
2017-04-01 21:40:09 +0200

    runnerty-executor-iterable

---
+++ executors/runnerty-executor-iterable/runnerty-executor-iterable.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var spawn = require("child_process").spawn;
+
+var Execution = require("../../classes/execution.js");
+
+class iterableExecutor extends Execution {
+  constructor(process) {
+    super(process);
+  }
+
+  exec() {
+    var _this = this;
+    var endOptions = {end: 'end'};
+
+    return new Promise(function (resolve, reject) {
+      _this.getValues()
+        .then((res) => {
+        endOptions.end = 'end';
+      endOptions.execute_return = JSON.stringify(res.objects);
+      _this.end(endOptions, resolve, reject);
+    })
+      .catch((err) => {
+        endOptions.end = 'error';
+      endOptions.messageLog = `ITERABLE Error getValues: ${err}`;
+      endOptions.execute_err_return = `ITERABLE Error getValues: ${err}`;
+      _this.end(endOptions, resolve, reject);
+    });
+    });
+  }
+}
+
+module.exports = iterableExecutor;
