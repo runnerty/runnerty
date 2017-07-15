a3f478f6c06a9261f6e66968cdc8e4aea3c6da26
carlos <carloscarmonaalcantara@gmail.com>
2017-07-15 22:07:31 +0200

    fix: refactor api

--- ws-api/ws-api.js
+++ ws-api/ws-api.js
@@ -1,19 +1,16 @@
 "use strict";
 
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
 var router = express.Router();
-var morgan = require('morgan');
-var jwt = require('jsonwebtoken');
-var expressJwt = require('express-jwt');
+var morgan = require("morgan");
+var jwt = require("jsonwebtoken");
+var expressJwt = require("express-jwt");
 var app = express();
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var helmet = require('helmet');
+var server = require("http").Server(app);
+var helmet = require("helmet");
 var utils = require("../lib/utils.js");
 var logger = utils.logger;
-var crypto = require('crypto');
 var config = global.config.general;
 const port = config.api.port;
 /*
@@ -21,18 +18,17 @@ const port = config.api.port;
  */
 
 //============================================
-var globalPlanChains = global.runtimePlan.plan;
+var apiPlan = global.runtimePlan.plan;
 
 module.exports = function () {
   //==============================================
   // SERVER
   server.listen(port, function (err, res) {
-    //TODO CATCH ERRORS:
-    logger.log('info', 'Listening on port [' + port + ']');
+    logger.log("info", "Listening on port [" + port + "]");
   });
 
   app.use(function (req, res, next) {
-    res.header("Content-Type", 'application/json');
+    res.header("Content-Type", "application/json");
     next();
   });
 
@@ -66,7 +62,7 @@ module.exports = function () {
     };
   }
 
-  app.set('json replacer', excluder);
+  app.set("json replacer", excluder);
 
 
   app.use(bodyParser.urlencoded({extended: true}));
@@ -75,7 +71,7 @@ module.exports = function () {
   //==============================================
   // SECURITY
   app.use(helmet());
-  app.disable('x-powered-by');
+  app.disable("x-powered-by");
   /*
    app.use(lusca({
    csp: {},
@@ -94,32 +90,6 @@ module.exports = function () {
    });
    */
 
-  //JSON Vulnerability Protection (Angular):
-  /*
-   app.use(function (req, res, next){
-
-   var actualSend = res.send;
-   res.send = function (data) {
-
-   var excludeJSONProtect = false;
-   for (var i = 0; i < config.global.excludeJSONProtectURLs.length; i++) {
-   if (req.originalUrl.indexOf(config.global.excludeJSONProtectURLs[i]) !== -1)
-   {
-   excludeJSONProtect = true;
-   }
-   };
-
-   if (typeof data === "object" && !excludeJSONProtect) {
-   var strData = ")]}',\n" + JSON.stringify(data);
-   res.set('Content-Type', 'text/json');
-   actualSend.call (res, strData);
-   } else {
-   actualSend.call (res, data);
-   }
-   };
-   next();
-   });
-   */
   /*
    app.use(function (req, res, next) {
 
@@ -143,39 +113,39 @@ module.exports = function () {
   //==================================================================
   // API
 
-  app.use(morgan('dev'));
+  app.use(morgan("dev"));
 
   app.use(bodyParser.json());
 
   app.use(expressJwt({
     secret: config.api.secret,
     getToken: function fromHeaderOrQuerystring(req) {
-      if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-        return req.headers.authorization.split(' ')[1];
+      if (req.headers.authorization && req.headers.authorization.split(" ")[0] === "Bearer") {
+        return req.headers.authorization.split(" ")[1];
       } else if (req.query && req.query.token) {
         return req.query.token;
       }
       return null;
     }
   }).unless({
-    path: ['/auth']
+    path: ["/auth"]
   }));
 
   app.use(function (err, req, res, next) {
-    if (err.name === 'UnauthorizedError') {
-      res.status(401).send('Unauthorized');
+    if (err.name === "UnauthorizedError") {
+      res.status(401).send("Unauthorized");
     }
   });
 
-  app.use('/', router);
+  app.use("/", router);
 
-  router.post('/auth', function (req, res) {
+  router.post("/auth", function (req, res) {
 
     var user = req.body.user;
     var password = req.body.password;
 
     if (!user) {
-      res.json({success: false, message: 'Authentication failed. User not found.'});
+      res.json({success: false, message: "Authentication failed. User not found."});
     } else if (user) {
 
       function checkAcces(up) {
@@ -190,38 +160,37 @@ module.exports = function () {
 
         res.json({
           success: true,
-          message: 'Run your token!',
+          message: "Run your token!",
           token: token
         });
 
       } else {
-        res.json({success: false, message: 'Authentication failed.'});
+        res.json({success: false, message: "Authentication failed."});
       }
     }
   });
 
   // GET ALL CHAINS
-  router.get('/chains', function (req, res) {
+  router.get("/chains", function (req, res) {
 
-    let objectToResult = ['depends_chains', 'args', 'events', 'output', 'chain_values', 'schedule_interval', 'scheduleCancel', 'scheduleRepeater', 'parentUId', 'exec', 'depends_process', 'retries', 'retry_delay', 'end_on_fail', 'end_chain_on_fail'];
+    let objectToResult = ["depends_chains", "args", "events", "output", "chain_values", "schedule_interval", "scheduleCancel", "scheduleRepeater", "parentUId", "exec", "depends_process", "retries", "retry_delay", "end_on_fail", "end_chain_on_fail"];
 
     function excluderGetChain(key, value) {
-      return value;
       if (objectToResult.indexOf(key) !== -1) {
         return undefined;
       }
       return value;
     }
 
-    res.send(JSON.stringify(globalPlanChains.chains, serializer(excluderGetChain)));
+    res.send(JSON.stringify(apiPlan.chains, serializer(excluderGetChain)));
   });
 
   // GET A CHAIN
-  router.get('/chain/:chainId', function (req, res) {
+  router.get("/chain/:chainId", function (req, res) {
     var chainId = req.params.chainId;
-    var chain = globalPlanChains.getChainById(chainId);
+    var chain = apiPlan.getChainById(chainId);
 
-    let objectToResult = ['depends_chains', 'args', 'events', 'output', 'chain_values', 'schedule_interval', 'scheduleCancel', 'scheduleRepeater', 'parentUId', 'exec', 'depends_process', 'retries', 'retry_delay', 'end_on_fail', 'end_chain_on_fail'];
+    let objectToResult = ["depends_chains", "args", "events", "output", "chain_values", "schedule_interval", "scheduleCancel", "scheduleRepeater", "parentUId", "exec", "depends_process", "retries", "retry_delay", "end_on_fail", "end_chain_on_fail"];
 
     function excluderGetChain(key, value) {
       if (objectToResult.indexOf(key) !== -1) {
@@ -238,56 +207,38 @@ module.exports = function () {
   });
 
   // GET A CHAIN
-  router.post('/chain/forceStart/:chainId', function (req, res) {
+  router.post("/chain/forceStart/:chainId", (req, res) => {
     var chainId = req.params.chainId;
-    var chain = globalPlanChains.getChainById(chainId);
+    var inputValues = null;
+    var customValues = {};
 
-    if (chain) {
-      var inputIterableValues;
+    if (req.body.inputIterableValues) inputValues = req.body.inputIterableValues;
 
-      var customValues = {};
-      if (req.body.hasOwnProperty('customValues')) {
-
-        try {
-          customValues = JSON.parse(req.body.customValues);
-        } catch (err) {
-          var newErr = new Error('Problem reading JSON file');
-          newErr.stack += '\nCaused by: ' + err.stack;
-          throw newErr;
-        }
+    if (req.body.customValues) {
+      try {
+        customValues = JSON.parse(req.body.customValues);
+      } catch (err) {
+        res.status(500).send("Error parsing customValues");
+        var newErr = new Error("Error parsing customValues");
+        newErr.stack += "\nCaused by: " + err.stack;
+        throw newErr;
       }
-
-      if (chain.iterable) {
-
-        crypto.randomBytes(16, function (err, buffer) {
-          var vProcUId = chainId + '_VP_' + buffer.toString('hex');
-
-          if (req.body.hasOwnProperty('inputIterableValues')) {
-            inputIterableValues = req.body.inputIterableValues;
-          }
-
-          var dummy_process = {};
-          dummy_process.uId = vProcUId;
-          dummy_process.childs_chains = [];
-          globalPlanChains.scheduleChain(chain, dummy_process, true, inputIterableValues, customValues);
-          res.json(`Chain iterable: "${chain.id}" starting.`);
-
-        });
-
-      } else {
-        globalPlanChains.scheduleChain(chain, undefined, true, undefined, customValues);
-        res.json(`Chain ${chain.id}/${chain.uId} starting.`);
-      }
-    } else {
-      res.status(404).send(`Chain "${chainId}" not found`);
     }
 
+    apiPlan.startChain(chainId, inputValues, customValues)
+      .then(() => {
+        res.json("");
+      })
+      .catch((err) => {
+        res.status(404).send(err);
+        logger.log("error", "forceStart error", err);
+      });
   });
 
   //GET ALL PROCESSES OF CHAIN INDICATED IN PARAMETER chainId
-  router.get('/processes/:chainId', function (req, res) {
+  router.get("/processes/:chainId", function (req, res) {
     var chainId = req.params.chainId;
-    var chain = globalPlanChains.getChainById(chainId);
+    var chain = apiPlan.getChainById(chainId);
 
     if (chain) {
       res.json(chain.processes);
@@ -299,28 +250,25 @@ module.exports = function () {
 
 
   // KILL A NO ITERABLE CHAIN INDICATE: chainId
-  router.post('/chain/stop/:chainId', function (req, res) {
+  router.post("/chain/stop/:chainId", function (req, res) {
     var chainId = req.params.chainId;
-
-    logger.log('info', `Kill chain "${chainId}" by ${req.user}`);
-
-    var chain = globalPlanChains.getChainById(chainId);
-
-    if (chain) {
-      chain.stop();
-      res.json();
-    } else {
-      res.status(404).send(`Chain "${chainId}" not found`);
-    }
-
+    apiPlan.stopChain(chainId)
+      .then(() => {
+        logger.log("info", `Kill chain "${chainId}" by ${req.user}`);
+        res.json("");
+      })
+      .catch((err) => {
+        res.status(404).send(err);
+        logger.log("error", "loadChainToPlan scheduleChain", err);
+      });
   });
 
 
   //GET A PROCESS OF CHAIN INDICATED IN PARAMETER chainId AND processId
-  router.get('/process/:chainId/:processId', function (req, res) {
+  router.get("/process/:chainId/:processId", function (req, res) {
     var chainId = req.params.chainId;
     var processId = req.params.processId;
-    var chain = globalPlanChains.getChainById(chainId);
+    var chain = apiPlan.getChainById(chainId);
 
     if (chain) {
       var process = chain.getProcessById(processId);
@@ -336,27 +284,26 @@ module.exports = function () {
   });
 
   // RETRY A PROCESS OF A CHAIN INDICATE: chainId, processId AND once (TRUE FOR ONCE RETRY FALSE FOR CONFIGURED RETRIES)
-  router.post('/process/retry', function (req, res) {
+  router.post("/process/retry", function (req, res) {
 
     var chainId = req.body.chainId;
     var processId = req.body.processId;
     var once = req.body.once || false;
 
-    logger.log('info', `Retrying process "${processId}" of chain "${chainId}" by ${req.user}`);
+    logger.log("info", `Retrying process "${processId}" of chain "${chainId}" by ${req.user}`);
 
-    var chain = globalPlanChains.getChainById(chainId);
+    var chain = apiPlan.getChainById(chainId);
 
     if (chain) {
       var process = chain.getProcessById(processId);
       if (process) {
         if (process.isErrored()) {
-          res.json();
+          res.json("");
           process.start(true, once)
-            .then(function (res) {
-            })
+            .then({})
             .catch(function (e) {
-              logger.log('error', 'Retrying process:' + e)
-            })
+              logger.log("error", "Retrying process:" + e);
+            });
         } else {
           res.status(423).send(`Process "${processId}" of chain "${chainId}" is not in errored status`);
         }
@@ -370,14 +317,14 @@ module.exports = function () {
   });
 
   // SET END A PROCESS OF A CHAIN INDICATE: chainId, processId
-  router.post('/process/end', function (req, res) {
+  router.post("/process/end", function (req, res) {
     var chainId = req.body.chainId;
     var processId = req.body.processId;
     var continueChain = req.body.continueChain || false;
 
-    logger.log('info', `Set end process "${processId}" of chain "${chainId}" by ${req.user}`);
+    logger.log("info", `Set end process "${processId}" of chain "${chainId}" by ${req.user}`);
 
-    var chain = globalPlanChains.getChainById(chainId);
+    var chain = apiPlan.getChainById(chainId);
 
     if (chain) {
       var process = chain.getProcessById(processId);
@@ -385,16 +332,16 @@ module.exports = function () {
         if (!process.isEnded() && !process.isRunning()) {
           res.json();
 
-          process.execute_return = '';
-          process.execute_err_return = '';
+          process.execute_return = "";
+          process.execute_err_return = "";
           process.end().then(() => {});
 
           if (continueChain) {
             chain.startProcesses()
               .then(function (res) {})
               .catch(function (err) {
-                logger.log('error', `Error in startProcesses next to set end process "${processId}" of chain "${chainId}"  by ${req.user}:` + err);
-              })
+                logger.log("error", `Error in startProcesses next to set end process "${processId}" of chain "${chainId}"  by ${req.user}:` + err);
+              });
           }
         } else {
           res.status(423).send(`Is not posible set process "${processId}" of chain "${chainId}" to end because is ${process.status}`);
@@ -409,20 +356,20 @@ module.exports = function () {
   });
 
   // KILL A PROCESS OF A CHAIN INDICATE: chainId, processId
-  router.post('/process/kill', function (req, res) {
+  router.post("/process/kill", function (req, res) {
     var chainId = req.body.chainId;
     var processId = req.body.processId;
 
-    logger.log('info', `Kill process "${processId}" of chain "${chainId}" by ${req.user}`);
+    logger.log("info", `Kill process "${processId}" of chain "${chainId}" by ${req.user}`);
 
-    var chain = globalPlanChains.getChainById(chainId);
+    var chain = apiPlan.getChainById(chainId);
 
     if (chain) {
       var process = chain.getProcessById(processId);
       if (process) {
         if (process.isRunning()) {
           res.json();
-          process.stop(req.user + ' REQUEST KILL PROCESS ' + processId + ' FROM CHAIN ' + chainId);
+          process.stop(req.user + " REQUEST KILL PROCESS " + processId + " FROM CHAIN " + chainId);
 
         } else {
           res.status(423).send(`Is not posible kill process "${processId}" of chain "${chainId}" to end because is ${process.status}`);
@@ -437,14 +384,14 @@ module.exports = function () {
   });
 
   // LOAD/REALOAD CHAIN
-  router.post('/chain/load', function (req, res) {
+  router.post("/chain/load", function (req, res) {
 
     var chainId = req.body.chainId;
     var planFile = req.body.planFile || config.planFilePath;
 
-    globalPlanChains.loadFileContent(planFile)
+    apiPlan.loadFileContent(planFile)
       .then((fileRes) => {
-        globalPlanChains.getChains(fileRes)
+        apiPlan.getChains(fileRes)
           .then((fileChains) => {
 
             var newChain = fileChains.find(function byId(chain) {
@@ -452,16 +399,16 @@ module.exports = function () {
             });
 
             if (newChain) {
-              globalPlanChains.loadChain(newChain)
+              apiPlan.loadChain(newChain)
                 .then(function (newChainObj) {
                   res.json();
-                  globalPlanChains.loadChainToPlan(newChainObj);
+                  apiPlan.loadChainToPlan(newChainObj);
                   // Force refresh binBackup
-                  globalPlanChains.refreshBinBackup();
+                  apiPlan.refreshBinBackup();
                 })
                 .catch(function (err) {
                   res.status(500).send(`Error loading "${chainId}":` + err);
-                  logger.log('error', 'FilePlan new Plan: ' + err);
+                  logger.log("error", "FilePlan new Plan: " + err);
                 });
             } else {
               res.status(404).send(`Chain "${chainId}" not found in file`);
@@ -469,23 +416,23 @@ module.exports = function () {
           })
           .catch(function (err) {
             res.status(500).send(`Error loading file chain "${chainId}":` + err);
-            logger.log('error', 'FilePlan loadFileContent getChains: ' + err);
+            logger.log("error", "FilePlan loadFileContent getChains: " + err);
           });
       })
       .catch(function (err) {
         res.status(500).send(`Error loading "${chainId}" (loading file):` + err);
-        logger.log('error', 'File Plan, constructor:' + err)
+        logger.log("error", "File Plan, constructor:" + err);
       });
   });
 
   // REMOVE CHAIN
-  router.post('/chain/remove', function (req, res) {
+  router.post("/chain/remove", function (req, res) {
     var chainId = req.body.chainId;
-    var chain = globalPlanChains.getChainById(chainId);
+    var chain = apiPlan.getChainById(chainId);
     if (chain) {
       if (!chain.isEnded() && !chain.isRunning()) {
         res.json();
-        globalPlanChains.chains.splice(globalPlanChains.getIndexChainById(chainId), 1);
+        apiPlan.chains.splice(apiPlan.getIndexChainById(chainId), 1);
       } else {
         res.status(423).send(`Is not posible remove chain "${chainId}" because is ${chain.status}`);
       }
