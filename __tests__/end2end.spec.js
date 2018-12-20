d9ee9204d20485d5a612cad8ea14e8d5b1b9d85c
José Antonio Ruiz <jose@coderty.com>
2018-12-20 14:36:02 +0100

    feat(tests): add iter fail and end chain test

--- __tests__/end2end.spec.js
+++ __tests__/end2end.spec.js
@@ -4,8 +4,8 @@ describe('Queues', () => {
   jest.setTimeout(10000);
 
   const successOutput = `info: 1 CHAIN_ONE
-info: 2 CHAIN_ONE_PROC_ONE QUEUE
-info: 3 CHAIN_TWO
+info: CHAIN
+info: CHAIN
 info: 4 CHAIN_TWO_PROC_ONE
 info: 5 CHAIN_TWO_PROC_TWO
 info: 6 CHAIN_TWO END
@@ -87,6 +87,61 @@ describe('SimpleIter', () => {
         './__tests__/end2end/config.json',
         '-P',
         './__tests__/end2end/plan_simple_iter.json',
+        '-f',
+        'CHAIN-LAUNCHER'
+      ],
+      9000,
+      res => {
+        const _res = res.substring(res.indexOf('\n') + 1);
+        expect(_res.replace(/(\r\n\t|\n|\r\t|\ )/gm, '')).toEqual(
+          successOutput.replace(/(\r\n\t|\n|\r\t|\ )/gm, '')
+        );
+        done();
+      }
+    );
+  });
+});
+
+describe('SimpleIterFail', () => {
+  jest.setTimeout(10000);
+
+  const successOutput = `info: 1 CHAIN CHAIN-LAUNCHER START
+  info: 2   PROCESS PROCESS-LAUNCHER OF CHAIN CHAIN-LAUNCHER START
+  info: 3    PROCESS PROCESS-LAUNCHER OF CHAIN CHAIN-LAUNCHER END
+  info: - 4     [I:echo PROCESS-LAUNCHER_1] CHAIN CHAIN-ITERABLE START
+  info: - 5       [I:echo PROCESS-LAUNCHER_1] PROCESS PROCESS-ITER-ONE OF CHAIN CHAIN-ITERABLE START
+  info: - 6       [I:echo PROCESS-LAUNCHER_1] PROCESS PROCESS-ITER-ONE OF CHAIN CHAIN-ITERABLE END
+  info: - 7      [I:echo PROCESS-LAUNCHER_1] PROCESS PROCESS-ITER-TWO OF CHAIN CHAIN-ITERABLE START
+  info: - 8      [I:echo PROCESS-LAUNCHER_1] PROCESS PROCESS-ITER-TWO OF CHAIN CHAIN-ITERABLE END
+  info: - 9   [I:echo PROCESS-LAUNCHER_1] CHAIN CHAIN-ITERABLE END
+  info: - 4     [I:echo PROCESS-LAUNCHER_2] CHAIN CHAIN-ITERABLE START
+  info: - 5       [I:echo PROCESS-LAUNCHER_2] PROCESS PROCESS-ITER-ONE OF CHAIN CHAIN-ITERABLE START
+  info: - 6       [I:echo PROCESS-LAUNCHER_2] PROCESS PROCESS-ITER-ONE OF CHAIN CHAIN-ITERABLE END
+  info: - 7      [I:echo PROCESS-LAUNCHER_2] PROCESS PROCESS-ITER-TWO OF CHAIN CHAIN-ITERABLE START
+  info: - 8      [I:echo PROCESS-LAUNCHER_2] PROCESS PROCESS-ITER-TWO OF CHAIN CHAIN-ITERABLE END
+  info: - 9   [I:echo PROCESS-LAUNCHER_2] CHAIN CHAIN-ITERABLE END
+  info: - 4     [I:echo PROCESS-LAUNCHER_3] CHAIN CHAIN-ITERABLE START
+  info: - 5       [I:echo PROCESS-LAUNCHER_3] PROCESS PROCESS-ITER-ONE OF CHAIN CHAIN-ITERABLE START
+  info: - 6       [I:echo PROCESS-LAUNCHER_3] PROCESS PROCESS-ITER-ONE OF CHAIN CHAIN-ITERABLE END
+  info: - 7      [I:echo PROCESS-LAUNCHER_3] PROCESS PROCESS-ITER-TWO OF CHAIN CHAIN-ITERABLE START
+  info: - 8      [I:echo PROCESS-LAUNCHER_3] PROCESS PROCESS-ITER-TWO OF CHAIN CHAIN-ITERABLE END
+  info: - 9   [I:echo PROCESS-LAUNCHER_3] CHAIN CHAIN-ITERABLE END
+  info: - 4     [I:lol PROCESS-LAUNCHER_4] CHAIN CHAIN-ITERABLE START
+  info: - 5       [I:lol PROCESS-LAUNCHER_4] PROCESS PROCESS-ITER-ONE OF CHAIN CHAIN-ITERABLE START
+  info: - 6       [I:lol PROCESS-LAUNCHER_4] PROCESS PROCESS-ITER-ONE OF CHAIN CHAIN-ITERABLE END
+  info: - 7      [I:lol PROCESS-LAUNCHER_4] PROCESS PROCESS-ITER-TWO OF CHAIN CHAIN-ITERABLE START
+  info: - 9   [I:lol PROCESS-LAUNCHER_4] CHAIN CHAIN-ITERABLE END
+  info: ERR!        [I:lol PROCESS-LAUNCHER_4] PROCESS PROCESS-ITER-TWO OF CHAIN CHAIN-ITERABLE FAIL: /bin/sh:lol:commandnotfound`;
+  
+  test('Execution End2End: SimpleIterFail', done => {
+    exec(
+      'node',
+      [
+        'index.js',
+        '-c',
+        './__tests__/end2end/config.json',
+        '-P',
+        './__tests__/end2end/plan_simple_iter_fail.json',
         '-f',
         'CHAIN-LAUNCHER'
       ],
